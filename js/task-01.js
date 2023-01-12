74dd8e8801ebe9b6013f3c89a959f95eb3af992e
Zucela <101594376+ZuzannaCelnik@users.noreply.github.com>
2023-01-12 03:03:08 +0100

    trial 1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const kot = document.querySelectorAll("#cat")
+
+const wertyu = document.querySelectorAll(".item")
+
+const wertyu2 = Array.from(wertyu)
+
+//console.log(wertyu)
+//console.log("cat")
+
+const categories = document.querySelectorAll(.item);
+console.log(`Number of categories: ${categories}`)
+
+document.querySelectorAll(.li.h2)
