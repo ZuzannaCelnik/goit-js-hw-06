c7e4de4ed03f0bb3bb20edfbb230d379ab1ac0f7
Zucela <101594376+ZuzannaCelnik@users.noreply.github.com>
2023-01-13 04:10:24 +0100

    01 fin

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,9 @@
-const kot = document.querySelectorAll("#cat")
+const categories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categories.length}`)
 
-const wertyu = document.querySelectorAll(".item")
-
-const wertyu2 = Array.from(wertyu)
-
-//wertyu.forEach((el, i) => (el.textContent="new"+i))
-
-//wertyu[2].textContent="loka"
-
-console.log(wertyu)
-console.log(kot)
-
-
-
-//const categories = document.querySelectorAll(.item);
-//console.log(`Number of categories: ${categories}`)
-
-//document.querySelectorAll(.li.h2)
-
-const myNewP = document.createElement("p")
-
-myNewP.textContent="Lorem ipsum"
+const itemsList = document.querySelectorAll('li.item')
+itemsList.forEach((el) => {
+    console.log(`Category: ${el.firstElementChild.textContent}`);
+    console.log(`Elements: ${el.lastElementChild.children.length}`)
+})
+    
