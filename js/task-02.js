f0169fb50be1d119a65ed0d0d50441e1bbf65066
Zucela <101594376+ZuzannaCelnik@users.noreply.github.com>
2023-01-13 04:29:35 +0100

    02 fin

--- js/task-02.js
+++ js/task-02.js
@@ -6,4 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients')
 
+const itemsArray=[]
+
+ingredients.forEach((el) => {
+  const ingrid = document.createElement('li')
+  ingrid.className='item'
+  ingrid.textContent = el
+  itemsArray.push(ingrid)
+})
+
+list.append(...itemsArray)
