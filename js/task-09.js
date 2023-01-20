006036128967a3c4eec8e2228833d2c7278df7dd
Zucela <101594376+ZuzannaCelnik@users.noreply.github.com>
2023-01-20 06:01:33 +0100

    09 fin

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = document.querySelector('.change-color');
+const color = document.querySelector('span.color')
+const body=document.querySelector('body')
+
+changeColor.addEventListener('click', (e) => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+})
