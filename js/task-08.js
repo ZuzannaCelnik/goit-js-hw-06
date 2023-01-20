63a671082cf1e881357bed6f3557898926c17004
Zucela <101594376+ZuzannaCelnik@users.noreply.github.com>
2023-01-20 05:50:35 +0100

    08 fin

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+const input=document.querySelectorAll('input')
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const {
+        elements: { email, password }
+    } = e.currentTarget
+    
+    if (email.value==='' || password.value==='') {
+        alert("Fill the gaps")
+    }
+  
+    const userInput = {
+        email: email.value,
+        password: password.value
+    }
+    console.log(userInput)
+    form.reset();
+})
